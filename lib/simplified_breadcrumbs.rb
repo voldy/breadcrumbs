ee63d6125298107205fc8696ccfbad88ad855a08
Francesc Esplugas <fesplugas@intraducibles.net>
2009-04-03 00:42:41 +0200

    Small cleanup.

--- lib/simplified_breadcrumbs.rb
+++ lib/simplified_breadcrumbs.rb
@@ -4,14 +4,14 @@ module SimplifiedBreadcrumbs
 
     protected
 
-    def add_breadcrumb name, url = ''
+    def add_breadcrumb(name, url = '')
       @breadcrumbs ||= []
       url = eval(url) if url =~ /_path|_url/
       @breadcrumbs << [name, url]
     end
 
-    def self.add_breadcrumb name, url, options = {}
-      before_filter options do |controller|
+    def self.add_breadcrumb(name, url = '', options = {})
+      before_filter(options) do |controller|
         controller.send(:add_breadcrumb, name, url)
       end
     end
@@ -21,7 +21,7 @@ module SimplifiedBreadcrumbs
   module Helper
 
     def breadcrumbs(separator = "&rsaquo;")
-      @breadcrumbs.map { |txt, path| link_to_unless(path.blank?, h(txt), path) }.join(" #{separator} ")
+      @breadcrumbs.map { |name, path| link_to_unless_current(name, path) }.join(" #{separator} ")
     end
 
   end
