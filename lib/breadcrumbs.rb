b12d14b68d705a7444713d4a215ea9eeea08b005
Francesc Esplugas <fesplugas@intraducibles.net>
2009-04-23 14:55:10 +0200

    Small cleanup.

--- lib/breadcrumbs.rb
+++ lib/breadcrumbs.rb
@@ -1,25 +1,33 @@
 module Breadcrumbs
+
   module InstanceMethods
+
     protected
+
     # Append a breadcrumb to the end of the trail
     def add_breadcrumb(name, url =  nil)
       @breadcrumbs ||= []
       url = send(url) if url.is_a?(Symbol)
       name = send(name).to_s.titleize if name.is_a?(Symbol)
       @breadcrumbs << [name, url]
-    end  
+    end
+
   end
-  
+
   module ClassMethods
-    # Append a breadcrumb to the end of the trail by deferring evaluation until the filter processing.
+
+    # Append a breadcrumb to the end of the trail by deferring evaluation 
+    # until the filter processing.
     def add_breadcrumb(name, url = nil, options = {})
       before_filter(options) do |controller|
         controller.send(:add_breadcrumb, name, url)
       end
-    end    
+    end
+
   end
 
   module HelperMethods
+
     # Returns HTML markup for the breadcrumbs
     def breadcrumbs(*args)
       default_options = {:separator => "&nbsp;&raquo;&nbsp;", :tag => :li}
@@ -29,7 +37,9 @@ module Breadcrumbs
         options[:tag] && content_tag(options[:tag], crumb) || crumb
       end.join("#{options[:separator]}")
     end
+
   end
+
 end
 
 class ActionController::Base
